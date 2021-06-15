34f9ca473d4a382ba81bf3be1edac24ab54168f6
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-15 11:50:20 -0300

    V1.0.0 First test version

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gabriel Gomes's MQTT Broker
+
+A private MQTT Broker, made in Node.js, to communicate IoT with the APIs
---
+++ src/App/index.js
@@ -0,0 +1,35 @@
+var net = require('net');
+var mqttConnection = require('mqtt-connection');
+var server = new net.Server();
+
+server.on('connection', (stream) => {
+
+    var client = mqttConnection(stream);
+
+    client.on('connect', function(packet){
+        console.log("client connected")
+        client.connack({returnCode:0});
+    });
+
+    client.on('publish', function(packet){
+        client.puback({messageId: packet.messageId});
+    });
+
+    client.on('pingreq', function(){
+        client.pingresp();
+    });
+
+    client.on('subscribe', function (packet) {
+        client.suback({ granted: [packet.qos], messageId: packet.messageId});
+    });
+
+    stream.setTimeout(1000 * 60 * 3);
+
+    client.on('close', function() { client.destroy() });
+    client.on('error', function() { client.destroy() });
+    client.on('disconnected', function() { client.destroy(); });
+
+    stream.on('timeout', function() { client.destroy() ;});
+});
+
+server.listen(1883);
