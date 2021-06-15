71386bac9df0238c5ef15fa7d255a14aa1c7afaa
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-15 12:42:44 -0300

    V1.0.1 Switching project structure

--- src/App/index.js
+++ src/App/index.js
@@ -1,35 +1,10 @@
-var net = require('net');
-var mqttConnection = require('mqtt-connection');
-var server = new net.Server();
+const aedes = require("aedes")();
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
+const port = process.env.PORT || 1883
 
-server.on('connection', (stream) => {
+ws.createServer({ server: httpServer }, aedes.handle);
 
-    var client = mqttConnection(stream);
-
-    client.on('connect', function(packet){
-        console.log("client connected")
-        client.connack({returnCode:0});
-    });
-
-    client.on('publish', function(packet){
-        client.puback({messageId: packet.messageId});
-    });
-
-    client.on('pingreq', function(){
-        client.pingresp();
-    });
-
-    client.on('subscribe', function (packet) {
-        client.suback({ granted: [packet.qos], messageId: packet.messageId});
-    });
-
-    stream.setTimeout(1000 * 60 * 3);
-
-    client.on('close', function() { client.destroy() });
-    client.on('error', function() { client.destroy() });
-    client.on('disconnected', function() { client.destroy(); });
-
-    stream.on('timeout', function() { client.destroy() ;});
+httpServer.listen(port, () => {
+    console.log("Server listening...")
 });
-
-server.listen(process.env.PORT || 1883);
