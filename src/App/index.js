0d37873f01ff20518e814f7a0974fa1b0c082610
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-29 19:21:32 -0300

    V1.0.2 Adding client connection emitter

--- src/App/index.js
+++ src/App/index.js
@@ -3,37 +3,10 @@ const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
 const port = process.env.PORT || 1883;
 
-const aedes = require("aedes")({
-    mq:mq
-});
+const aedes = require("aedes")();
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
 httpServer.listen(port, () => {
     console.log("Server listening...");
-});
-
-aedes.on('subscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-            '\x1b[0m subscribed to topics: ' + subscriptions.map(s => s.topic).join('\n'), 'from broker', aedes.id)
-});
-
-aedes.on('unsubscribe', function (subscriptions, client) {
-    console.log('MQTT client \x1b[32m' + (client ? client.id : client) +
-            '\x1b[0m unsubscribed to topics: ' + subscriptions.join('\n'), 'from broker', aedes.id)
-});
-
-// fired when a client connects
-aedes.on('client', function (client) {
-    console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
-});
-
-// fired when a client disconnects
-aedes.on('clientDisconnect', function (client) {
-    console.log('Client Disconnected: \x1b[31m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
-});
-
-// fired when a message is published
-aedes.on('publish', async function (packet, client) {
-    console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
 });
