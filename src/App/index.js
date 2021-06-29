f22b74138ebe2200f60b770d084fb1cbba00654c
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-29 19:58:50 -0300

    V1.0.4 Uploading server on one more than 1 port

--- src/App/index.js
+++ src/App/index.js
@@ -3,14 +3,9 @@ const { createServer } = require('aedes-server-factory')
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
 const port = process.env.PORT || 1883
-const server = createServer(aedes)
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
-server.listen(port, function () {
-  console.log('server started and listening on port ', port)
-})
-
-httpServer.listen(process.env.PORT || 3333, () => {
-    console.log("Server listening...");
+httpServer.listen(port, () => {
+    console.log("Server listening on port: ", port);
 });
