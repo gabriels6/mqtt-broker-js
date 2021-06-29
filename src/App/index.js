97e73c62ac3bed5182448e9c9fef77a17e92de87
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-29 18:59:53 -0300

    V1.0.2 Adding client connection emitter

--- src/App/index.js
+++ src/App/index.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 1883
 
 ws.createServer({ server: httpServer }, aedes.handle);
 
+aedes.on('client', function(client) {
+    console.log("Client connected! Id: " + client);
+
+    client.publish("Server accepted connection!");
+});
+
 httpServer.listen(port, () => {
-    console.log("Server listening...")
+    console.log("Server listening...");
 });
