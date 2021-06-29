36c2d47d3ca17ce78d39cec59708daa2dd7ac11e
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-29 19:46:35 -0300

    V1.0.3 Switching aedes MQTT to server-factory

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,23 @@
-const mq = require('mqemitter');
-const httpServer = require("http").createServer();
-const ws = require("websocket-stream");
-const port = process.env.PORT || 1883;
+const aedes = require('aedes')()
+const { createServer } = require('aedes-server-factory')
+const port = process.env.PORT || 1883
 
-const aedes = require("aedes")();
+const server = createServer(aedes)
 
-ws.createServer({ server: httpServer }, aedes.handle);
+server.listen(port, function () {
+  console.log('server started and listening on port ', port)
+})
 
-httpServer.listen(port, () => {
-    console.log("Server listening...");
-});
+
+// const mq = require('mqemitter');
+// const httpServer = require("http").createServer();
+// const ws = require("websocket-stream");
+// const port = process.env.PORT || 1883;
+
+// const aedes = require("aedes")();
+
+// ws.createServer({ server: httpServer }, aedes.handle);
+
+// httpServer.listen(port, () => {
+//     console.log("Server listening...");
+// });
