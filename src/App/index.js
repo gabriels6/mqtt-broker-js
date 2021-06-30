3aa7dd0243cf1abe67295dce1b7fb6d5d5594ba0
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-30 10:25:59 -0300

    V1.0.5 Switching server type

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,14 @@
 const aedes = require('aedes')()
 const { createServer } = require('aedes-server-factory')
-const httpServer = require("http").createServer();
-const ws = require("websocket-stream");
+//const httpServer = require("http").createServer();
+//const ws = require("websocket-stream");
 const port = process.env.PORT || 1883
 
-ws.createServer({ server: httpServer }, aedes.handle);
+//ws.createServer({ server: httpServer }, aedes.handle);
 
-httpServer.listen(port, () => {
+const Server = createServer(aedes)
+
+Server.listen(port, () => {
     console.log("Server listening on port: ", port);
 });
 
