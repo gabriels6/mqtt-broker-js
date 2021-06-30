e3eaea856719d32799602e474daef247de8b5889
Gabriel Gomes <gabrielsogom@gmail.com>
2021-06-29 21:52:34 -0300

    V1.0.5 Adding procfile

--- src/App/index.js
+++ src/App/index.js
@@ -9,3 +9,7 @@ ws.createServer({ server: httpServer }, aedes.handle);
 httpServer.listen(port, () => {
     console.log("Server listening on port: ", port);
 });
+
+aedes.on('client', function (client) {
+    console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
+});
