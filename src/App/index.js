eeabae5f75287f1321b4757c346a4c08238817db
Gabriel Gomes <gabrielsogom@gmail.com>
2021-07-01 19:21:15 -0300

    V1.0.7 Adding express to execute Server functions

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,23 @@
 const aedes = require('aedes')();
 const httpServer = require('http').createServer();
 const ws = require('websocket-stream');
+
+const cors = require("cors");
+const express = require("express");
+const routes = require("../Routes");
+
 const port = process.env.PORT || 1883;
 
-ws.createServer({server: httpServer }, aedes.handle);
+const app = express()
 
-httpServer.listen(port, () => {
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+ws.createServer({server: app }, aedes.handle);
+
+
+app.listen(port, () => {
     console.log("Server listening on port: ", port);
 });
 
