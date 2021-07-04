12843181f3af70657780ae7edf2f87fe85d4bbc1
Gabriel Gomes <gabrielsogom@gmail.com>
2021-07-04 18:01:39 -0300

    V1.0.9 Implementing broker with protocol decoder

--- src/App/index.js
+++ src/App/index.js
@@ -1,45 +1,51 @@
-const aedes = require('aedes')();
-const http = require('http')
-const ws = require('websocket-stream');
+const aedes = require('aedes');
+const { protocolDecoder } = require('aedes-protocol-decoder');
 
-const httpServer = http.createServer((req,res) => {
-    res.write('0');
-    res.end();
+const broker = aedes({
+    preConnect: function (client,packet,done){
+        if(client.connDetails && client.connDetails.ipAddress) {
+            client.ip = client.connDetails.ipAddress;
+        }
+        return done(null,true);
+    }
+})
+
+const httpServer = require('aedes-server-factory').createServer(broker,{
+    ws:true,
+    trustProxy:true,
+    protocolDecoder
 });
 
 const port = process.env.PORT || 1883;
-
-
-ws.createServer({server: httpServer }, aedes.handle);
 
 
 httpServer.listen(port, () => {
     console.log("Server listening on port: ", port);
 });
 
-aedes.on('client', function (client) {
+broker.on('client', function (client) {
     console.log('Client Connected: \x1b[33m' + (client ? client.id : client) + '\x1b[0m', 'to broker', aedes.id)
 });
 
-aedes.on('connectionError', function(client,error){
+broker.on('connectionError', function(client,error){
     console.log('Connection error on client: '+(client ? client.id : client)+'; Error:' + error)
 });
 
-aedes.on('clientError', function(client,error){
+broker.on('clientError', function(client,error){
     console.log('Client error on client: '+(client ? client.id : client)+'; Error:' + error)
 })
 
-aedes.authenticate = function(client,username,password,callback){
+broker.authenticate = function(client,username,password,callback){
 
     console.log('Client authenticated: ' + (client ? client.id : client));
 
     callback(null,true);
 }
 
-aedes.authorizePublish = function(client,packet,callback){
+broker.authorizePublish = function(client,packet,callback){
     callback(null);
 }
 
-aedes.authorizeSubscribe = function(client,subscription,callback){
+broker.authorizeSubscribe = function(client,subscription,callback){
     callback(null,subscription);
 }
